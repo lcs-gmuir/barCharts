edf25eba8b1a4c17361d334b95fca3826e50e972
Griffin Muir <gmuir@lcs.on.ca>
2024-02-04 22:34:40 -0500

    Initial Commit

---
+++ barCharts/barChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  barChartsApp.swift
+//  barCharts
+//
+//  Created by Griffin Muir on 2024-02-04.
+//
+
+import SwiftUI
+
+@main
+struct barChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
