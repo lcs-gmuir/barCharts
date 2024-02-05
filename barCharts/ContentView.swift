da34153ccc0373f5941e6fc7904bc520c57fde96
Griffin Muir <gmuir@lcs.on.ca>
2024-02-04 22:40:25 -0500

    made a bar chart that uses stored data

--- barCharts/ContentView.swift
+++ barCharts/ContentView.swift
@@ -2,17 +2,14 @@ import SwiftUI
 import Charts
 
 struct ContentView: View {
+    let weekdays = Calendar.current.shortWeekdaySymbols
+    let steps = [ 10531, 6019, 7200, 8311, 7403, 6503, 9230 ]
+
     var body: some View {
         Chart {
-            BarMark(
-                x: .value("Day", "Monday"),
-                y: .value("Steps", 6019)
-            )
-
-            BarMark(
-                x: .value("Day", "Tuesday"),
-                y: .value("Steps", 7200)
-            )
+            ForEach(weekdays.indices, id: \.self) { index in
+                BarMark(x: .value("Day", weekdays[index]), y: .value("Steps", steps[index]))
+            }
         }
     }
 }
