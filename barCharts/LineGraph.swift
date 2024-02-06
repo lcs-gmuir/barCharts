887eff0b8eebe1897f712d3022f6d994ee94df36
Griffin Muir <gmuir@lcs.on.ca>
2024-02-06 14:39:37 -0500

    added a linegraph view file seperate from the bar graph view to display weather data

---
+++ barCharts/LineGraph.swift
@@ -0,0 +1,18 @@
+//
+//  LineGraph.swift
+//  barCharts
+//
+//  Created by Griffin Muir on 2024-02-06.
+//
+
+import SwiftUI
+
+struct LineGraph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineGraph()
+}
